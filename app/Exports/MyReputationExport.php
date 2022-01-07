a78115e1c329bca2ecfd871443c3e9baeb2799c3
ledgerleap1 <micky@ledgerleap.com>
2022-01-07 13:05:11 -0500

    GP 373

--- app/Exports/MyReputationExport.php
+++ app/Exports/MyReputationExport.php
@@ -7,10 +7,13 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class MyReputationExport implements FromCollection, WithHeadings, WithMapping, WithCustomCsvSettings, WithColumnFormatting
+class MyReputationExport implements FromCollection, WithHeadings, WithMapping, WithCustomCsvSettings, WithColumnFormatting, ShouldAutoSize, WithEvents
 {
     public function __construct($query)
     {
@@ -32,9 +35,9 @@ class MyReputationExport implements FromCollection, WithHeadings, WithMapping, W
             $reputation->event,
             $reputation->proposal_title,
             $reputation->type,
-            in_array($reputation->type, ['Gained', 'Minted', 'Stake Lost', 'Lost']) ?  ' ' . (string) number_format($reputation->value, 5) : '',
-            $reputation->type == 'Staked' ? ' ' . (string) number_format($reputation->staked, 5) : '',
-            $reputation->type == 'Minted Pending' ? ' ' . (string) number_format($reputation->pending, 5) : '',
+            in_array($reputation->type, ['Gained', 'Minted', 'Stake Lost', 'Lost']) ? number_format($reputation->value, 5) : null,
+            $reputation->type == 'Staked' ?  number_format($reputation->staked, 5) : null,
+            $reputation->type == 'Minted Pending'  ? number_format($reputation->pending, 5) : null,
             ' ' . $reputation->created_at->format('m-d-Y H:i A'),
         ];
     }
@@ -62,9 +65,21 @@ class MyReputationExport implements FromCollection, WithHeadings, WithMapping, W
     public function columnFormats(): array
     {
         return [
-            'D' => NumberFormat::FORMAT_TEXT,
-            'E' => NumberFormat::FORMAT_TEXT,
-            'F' => NumberFormat::FORMAT_TEXT,
+            'D' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED2,
+            'E' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED2,
+            'F' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED2,
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $event->sheet->setCellValue('D' . ($event->sheet->getHighestRow() + 1), '=SUM(D2:D' . $event->sheet->getHighestRow() . ')');
+                $event->sheet->setCellValue('E' . ($event->sheet->getHighestRow()), '=SUM(E2:E' . $event->sheet->getHighestRow() - 1 . ')');
+                $event->sheet->setCellValue('F' . ($event->sheet->getHighestRow()), '=SUM(F2:F' . $event->sheet->getHighestRow() - 1 . ')');
+                $event->sheet->setCellValue('A' . ($event->sheet->getHighestRow()), 'Total');
+            }
         ];
     }
 }
