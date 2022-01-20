fad4709f7d512a5a9a9488f9523ac3fa6269f43d
= <=>
2022-01-20 11:22:30 -0500

    forum_name to slug

--- app/Services/DiscourseService.php
+++ app/Services/DiscourseService.php
@@ -253,7 +253,7 @@ class DiscourseService
 
     public function getUsername(User $user)
     {
-        return $user->profile->forum_name;
+        return Str::slug($user->profile->forum_name);
     }
 
     private function json(ResponseInterface $response)
