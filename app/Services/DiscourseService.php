7e93db9edcd007f19e49c114b3c449aad208f6f5
= <=>
2022-01-27 14:55:22 -0500

    forum_name preg replace update

--- app/Services/DiscourseService.php
+++ app/Services/DiscourseService.php
@@ -316,9 +316,10 @@ class DiscourseService
 
     public function getUsername(User $user)
     {
-        // return Str::slug($user->profile->forum_name);
         $updated_forum_name = str_replace(' ', '-', $user->profile->forum_name);
-        return preg_replace("/([^A-Za-z0-9\-\_.])/", '', $updated_forum_name);
+        $updated_forum_name = preg_replace("/([^A-Za-z0-9\-\_.])/", '', $updated_forum_name);
+        $updated_forum_name = str_replace('--', '-', $updated_forum_name);
+        return strtolower($updated_forum_name);
     }
 
     private function json(ResponseInterface $response)
