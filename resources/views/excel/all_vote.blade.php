569fba524bd2b9d7c7ed81a058a88e30e2ed20e2
ledgerleap1 <micky@ledgerleap.com>
2022-02-27 08:41:17 -0500

    GP-431 TWO CSV download tools needed

---
+++ resources/views/excel/all_vote.blade.php
@@ -0,0 +1,34 @@
+<table>
+    <thead>
+
+    </thead>
+    <tbody>
+        <tr>
+            <td>Proposal number</td>
+            <td>Vote Type</td>
+            <td>Vote stage</td>
+            <td>Stake for/against</td>
+            @foreach($users as $user)
+            <td>{{ $user->forum_name }}</td>
+            @endforeach
+        </tr>
+        @foreach($votes as $vote)
+        <tr>
+            <td>{{ $vote->proposal_id }}</td>
+            <td>{{ $vote->content_type }}</td>
+            <td>{{ $vote->type}}</td>
+            <td>{{ $vote->for_value .'/' .$vote->against_value }}</td>
+            @foreach($vote->responseVotes as $response)
+            @if($response['type'] == 'for')
+            <td>{{ $response['value'] }}</td>
+            @elseif($response['type'] == 'against')
+            <td>  {{ - $response['value']}} </td>
+            @else
+            <td> {{$response['type']}}</td>
+            @endif
+            @endforeach
+        </tr>
+        @endforeach
+
+    </tbody>
+</table>
